7b962fd300a640382e50271a35d0681dac344997
kaylopezal <kaylopezal@gmail.com>
2020-07-06 20:21:30 -0500

    Se adiciona filtros al dash

--- app/App/app.py
+++ app/App/app.py
@@ -7,6 +7,8 @@ import plotly.express as px
 import pandas as pd
 import json
 import sys
+from datetime import datetime as dt
+
 #sys.path.insert(1, '/tabContents')
 
 
@@ -48,15 +50,126 @@ app.layout = html.Div([
 dbc.Row(
     [
     ###  create sidebar 
-    dbc.Col(
+    #dbc.Col([html.P('  '),]),
+	
+	dbc.Col(
     [
-        html.H2("Filters", className="display-4"),
+        html.H4("Filters"),
         html.Hr(),
         html.P(
-            "A simple sidebar layout with navigation links", className="lead"
+            "Prision income date range", className="lead"
         ),
+		dcc.DatePickerRange(
+							id='my-date-picker-range',
+							min_date_allowed=dt(2010, 1, 1),
+							max_date_allowed=dt(2021, 1, 1),
+							initial_visible_month=dt(2020, 7, 1)#,
+							#end_date=dt(2020, 7, 15).date()
+		),
+		html.P(
+            "                 ", className="lead"
+        ),
+		html.P(
+            "Reclusion Department", className="lead"
+        ),
+		dcc.Dropdown(
+        id='reclusion_dep',
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montreal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC'
+    ),
+	
+	html.P(
+            "                 ", className="lead"
+        ),
+		html.P(
+            "Crime", className="lead"
+        ),
+		dcc.Dropdown(
+        id='crime',
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montreal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC'
+    ),
+	
+	html.P(
+            "                 ", className="lead"
+        ),
+		html.P(
+            "Sentence Type", className="lead"
+        ),
+		dcc.Dropdown(
+        id='sentence_type',
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montreal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC'
+    ),
+	
+	html.P(
+            "                 ", className="lead"
+        ),
+		html.P(
+            "Gender", className="lead"
+        ),
+	
+	dcc.Checklist(
+		id='gender',
+		options=[
+			{'label': 'Male', 'value': '2'},
+			{'label': 'Female', 'value': '1'},
+		],
+		value=['1', '2'],
+		labelStyle={'display': 'inline-block', 'align': 'center', 'padding-left' : '30px'}
+),
+	html.P(
+            "                 ", className="lead"
+        ),
+		html.P(
+            "Range Age", className="lead"
+        ),
+	dcc.RangeSlider(
+					id='range_age',
+					count=1,
+					min=15,
+					max=100,
+					step=1,
+					value=[15, 100],
+					marks={15:'15', 20: '20', 30:'30', 40:'40',50:'50',60:'60',70:'70',80:'80',90:'90',100:'100'},
+					tooltip={'always_visible': True}
+
+), 
+
+html.P(
+            "                 ", className="lead"
+        ),
+		html.P(
+            "Exceptional Conditions", className="lead"
+        ),
+	
+	dcc.Checklist(
+		id='excep_cond',
+		options=[
+			{'label': 'Yes', 'value': '2'},
+		],
+		#value=['1', '2'],
+		labelStyle={'display': 'inline-block', 'align': 'center', 'padding-left' : '30px'}
+), 
+	
     ],
-    width=2
+    width={"size": 'auto'#,
+		   #"offset": 1
+		   },
+	style={'margin-left': '20px'},
+	#align="center"
 ),
     # create the app content, simply add every tab created in the folder
 dbc.Col([
