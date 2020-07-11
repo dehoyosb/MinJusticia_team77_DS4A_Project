23105fbe237e824c3b5c3e38cbb1ee0813ee26cd
esneyder <emguerrerog@gmail.com>
2020-07-11 18:48:11 -0500

    se adicionan los cambios realizados al ETL para la inclusión del los SDHI index y severity

--- app/backend/etl.py
+++ app/backend/etl.py
@@ -76,7 +76,73 @@ class ETL():
         data_reg['adulto_mayor'] = 'N'
         data_reg.loc[(data_reg['CONDIC_EXPECIONAL'].str.contains('ADULTO MAYOR')),'adulto_mayor'] =  'S'
         
+        #severity index
+
+                # Count number of observations by crime type and category
+        top10 = list(data.DELITO.value_counts().to_frame().reset_index().rename(columns = {'index':'DELITONAME'}).head(10)['DELITONAME'])
+        #https://leyes.co/codigo_penal.htm
+        #values as [min_month,max_month,decree]
+        dict_pena ={'HURTO': [16,108,239],
+                    'TRAFICO FABRICACION O PORTE DE ESTUPEFACIENTES': [128,360,376],
+                    'FABRICACION TRAFICO Y PORTE DE ARMAS DE FUEGO O MUNICIONES':[132,180,366],
+                    'CONCIERTO PARA DELINQUIR':[48,108,340],
+                    'HOMICIDIO':[208,450,103],
+                    'EXTORSION':[192,288,244],
+                    'FABRICACIÓN, TRÁFICO, PORTE O TENENCIA DE ARMAS DE FUEGO, ACCESORIOS, PARTES O MUNICIONES':[108,144,365],
+                    'VIOLENCIA INTRAFAMILIAR':[48,720,229],
+                    'LESIONES PERSONALES':[16,360,[111,112,113,114,115,116,117,118,119,120,121]],
+                    'FABRICACION  TRAFICO Y PORTE DE ARMAS Y MUNICIONES DE USO PRIVATIVO DE LAS FUERZAS ARMADAS':[132,180,366]
+                    }
+        df_severity = pd.DataFrame(dict_pena)
+        df_severity_t = df_severity.transpose().reset_index()
+        col_names=['crime','min_month','max_month','decree']
+        df_severity_t.columns = col_names
+        crime_score = {'HURTO':1,
+                       'TRAFICO FABRICACION O PORTE DE ESTUPEFACIENTES':7,
+                       'FABRICACION TRAFICO Y PORTE DE ARMAS DE FUEGO O MUNICIONES':4,
+                       'CONCIERTO PARA DELINQUIR':2,
+                       'HOMICIDIO':9, 
+                       'EXTORSION':6,
+                       'FABRICACIÓN, TRÁFICO, PORTE O TENENCIA DE ARMAS DE FUEGO, ACCESORIOS, PARTES O MUNICIONES':3,
+                       'VIOLENCIA INTRAFAMILIAR':10,
+                       'LESIONES PERSONALES':8,
+                       'FABRICACION  TRAFICO Y PORTE DE ARMAS Y MUNICIONES DE USO PRIVATIVO DE LAS FUERZAS ARMADAS':5
+                      }
+        data['crime_score'] = data['DELITO'].apply(lambda x: crime_score[x] if x in top10 else 0)
+        display(df_severity_t,data.head(1))
+        #Calificado,Agravado,tentativa
+        CAT_cases = data[['TENTATIVA','AGRAVADO', 'CALIFICADO']].drop_duplicates().reset_index(drop=True)
+        CAT_cases['multiplier']= 'TBD'
+        CAT_cases.loc[5,'multiplier'] = 1  # TENTATIVA
+        CAT_cases.loc[0,'multiplier'] = 2    # CONSUMADO
+        CAT_cases.loc[4,'multiplier'] = 3 # TENTATIVA-AGRAVADO
+        CAT_cases.loc[7,'multiplier'] = 4  # TENTATIVA-CALIFICADO
+        CAT_cases.loc[6,'multiplier'] = 5 # TENTATIVA-CALIFICADO-AGRAVADO
+        CAT_cases.loc[3,'multiplier'] = 6    # CONSUMADO-AGRAVADO
+        CAT_cases.loc[2,'multiplier'] = 7    # CONSUMADO-CALIFICADO
+        CAT_cases.loc[1,'multiplier'] = 8    # CONSUMADO-CALIFICADO-AGRAVADO
+        display(CAT_cases)
+        # Feature engineering
+        # Generate score
+        data['CONSUMADO_b']  = data.TENTATIVA .apply(lambda x: 4 if x=="N" else 0)
+        data['TENTATIVA_b'] = data.TENTATIVA .apply(lambda x: 3 if x=="S" else 0)
+        data['CALIFICADO_b'] = data.CALIFICADO.apply(lambda x: 2 if x=="S" else 0)
+        data['AGRAVADO_b ']  = data.AGRAVADO  .apply(lambda x: 1 if x=="S" else 0)
+        # Multiplier score
+        data['crime_multiplier'] = (data.CONSUMADO_b + data.TENTATIVA_b + data.CALIFICADO_b + data.AGRAVADO_b)/7
+        # Severity 
+        data['severity'] = data.crime_score*data.crime_multiplier
+        data_reg['severity'] = data.crime_score*data.crime_multiplier
+
         ############## registro
         data_reg.to_sql('registros_tmp', con=self.queries.engine)
         self.queries.run('select * from registros_tmp limit 5')
         self.queries.run('SELECT public.tcompararreg();')
+
+
+
+
+
+
+
+
