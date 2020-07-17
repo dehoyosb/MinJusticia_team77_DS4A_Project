4dc77e7b1c8c94f2921980bd8f4af243485c588a
dehoyosb <daniel.hoyos@mercadolibre.com.co>
2020-07-17 13:12:55 -0500

    adding modifiers to the queries, to storage them in the class. Fixing issues witht he severity score and the sdhi score code, which was adding errores to the application

--- app/backend/utils.py
+++ app/backend/utils.py
@@ -28,11 +28,20 @@ class Queries():
         self.engine = engine
         self.query_dict = {'encoding': 'select * from persona \
                                         left join registro on persona.id_persona = registro.persona_id_persona \
-                                        left join delito on delito.id_delito = registro.delito_id_delito'}
+                                        left join delito on delito.id_delito = registro.delito_id_delito',
+                           'etl_select_1': """select * from reconocimiento_etnico""",
+                           'etl_select_2': """select * from diversidad_sexual""",
+                           'etl_select_3': """select * from persona where diversidad_sexual = 2""",
+                           'etl_insert_1':'INSERT INTO public.persona_diversidad_sexual (id_persona, id_diversidad_sexual) VALUES({});',
+                           'etl_select_4':'select * from personas_tmp limit 5',
+                           'etl_select_5': 'SELECT public.tcompararpersonas();',
+                           'etl_select_6': 'SELECT public.tcompararreg();',
+                           'etl_select_7':'select * from departamento'}
     
     def run(self, sql):
         result = self.engine.connect().execution_options(isolation_level="AUTOCOMMIT").execute((text(self.query_dict[sql])))
         return pd.DataFrame(result.fetchall(), columns=result.keys())
     
     def insert(self, sql):
-        return self.engine.connect().execution_options(isolation_level="AUTOCOMMIT").execute((text(self.query_dict[sql])))
+        return self.engine.connect().execution_options(isolation_level="AUTOCOMMIT")\
+                .execute((text(sql)))
